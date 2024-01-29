b67172d646a74b17e9972731beff705c5e753582
Emmanuel Tawiah <tawiah.tetteh10@gmail.com>
2024-01-29 19:49:25 +0000

    added checked state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,47 @@
-import { useState } from 'react'
+import {useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+
 import './App.css'
 import EventList from "./EventList.tsx";
+import {Todo} from "./interfaces.ts";
+
 
 function App() {
+const [todo,setTodo] = useState<string>("");
+const[todoList,setTodoList] = useState<Todo[]>([]);
+
+function updateTodoList(newTodo :Todo) :void {
+    if(newTodo.todo.length < 3) return;
+    setTodoList([...todoList,newTodo]);
+    setTodo("");
+}
+
+function handleOnEventChecked(eventId: string) :void {
+   setTodoList(list=> {
+       return list.map(todo => {
+           if(todo.id === eventId) {
+               return {...todo,isChecked: !todo.isChecked}
+           } else return todo;
+       }
+       );
+   });
+}
 
   return (
       <div className="container">
         <div className="main">
             <h3>Todo App</h3>
-            <input id="noteInput" type="text" placeholder="What do you need to do?"/>
-            <EventList/>
+            <input id="noteInput" type="text" placeholder="What do you need to do?"
+                   value={todo}
+                   onChange={e=> setTodo(e.target.value)}
+                   onBlur={() =>  updateTodoList({id:uuidv4(),  todo, isChecked: false})
+               }/>
+            {todoList.length > 0 ? <EventList list={todoList} onEventChecked={handleOnEventChecked}/> : <p className="emptyResponse">No events added</p> }
+
             <hr/>
             <div className="bottom">
-                <button>Check All</button>
-                <p>0 items remaining</p>
+                <button onClick={() => {}}>Check All</button>
+                <p> items remaining</p>
             </div>
         </div>
       </div>
--- src/EventList.tsx
+++ src/EventList.tsx
@@ -1,18 +1,18 @@
 import {ReactElement} from "react";
+import {Todo} from "./interfaces.ts";
 
-export default function EventList() :ReactElement {
+export default function EventList({list,onEventChecked}) :ReactElement {
     return (
-        <div className="eventList">{Array.from({length:5},(_,i) =>  <Event/> )}</div>);
+    <div className="eventList">{list.map(el=> <Event key={el.id} id = {el.id} name={el.todo} isChecked={el.isChecked} onEventChecked={onEventChecked}/>)}</div>)
 }
 
-function Event() :ReactElement {
+function Event({id,name,isChecked,onEventChecked}) :ReactElement {
     return (
         <div className="event">
             <div className="checkItem">
-                <input type="checkbox"/>
-                <p>HIII</p>
+                <input type="checkbox" onChange={() => onEventChecked(id)}/>
+                <p style={{textDecoration : isChecked ? 'line-through' : ''}}>{name}</p>
             </div>
-            <p onClick={() => {
-            }}>&times;</p>
+            <p onClick={() => {}}>&times;</p>
         </div>)
 }
